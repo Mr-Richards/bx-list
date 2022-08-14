fa3c70cabb1224e17714608c29a4904f226ce641
Mr-Richards <alexpricerichards@gmail.com>
2022-08-14 14:16:18 +0100

    List is displaying all data

--- src/Components/List.js
+++ src/Components/List.js
@@ -1,7 +1,20 @@
 import React from "react";
+import "../App.css";
+import { bxData } from "../data";
 
-function List() {
-  return <div>List</div>;
+export function List() {
+  return (
+    <>
+      <div className="list-container">
+        {bxData.map((data, key) => {
+          return (
+            <div key={key}>
+              {" "}
+              {data.name + ", " + data.address + ", " + data.company}{" "}
+            </div>
+          );
+        })}
+      </div>
+    </>
+  );
 }
-
-export default List;
